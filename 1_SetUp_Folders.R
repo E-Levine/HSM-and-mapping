01e130388d83620666ba87dc92ff112039e696ea
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-01-06 08:19:54 -0500

    Update 1_SetUp_Folders.R
    Code updated for separation of KML files.

--- 1_SetUp_Folders.R
+++ 1_SetUp_Folders.R
@@ -1,16 +1,43 @@
-##Code to set up folders for HSM
+##Code to set up folders for HSM and separate KML files as needed
 #
 ##This code should be run before any other files or setup is completed.
 ##Determine if the site already exists locally. If so, make sure to use the same code and identify which version is being created. 
 #
 ##This code does NOT need to be run if updating an existing site/version. 
 #
-##Establish the site code and version for this project.
+##Establish the site code and version for this project. (Required for both code chunks.)
 #
-Site_code <- c("SL") #two-letter site code used throughout for identifying files
+Site_code <- c("TB") #two-letter site code used throughout for identifying files
 Version <- c("v1") #current version number of the model for the specified site
 #
 #
+####Folder set up####
 ##Code checks if folders exist before creating folders for organizing by site and version. 
 if(!dir.exists(paste0("Data/",Site_code, "_", Version))){dir.create(paste0("Data/",Site_code, "_", Version))}
 if(!dir.exists(paste0("Output/",Site_code, "_", Version))){dir.create(paste0("Output/",Site_code, "_", Version))}
+if(!dir.exists(paste0("Reference files/KML/",Site_code, "_", Version))){dir.create(paste0("Output/",Site_code, "_", Version))}
+#
+#
+#
+#
+#
+#
+####File separation####
+#
+##Ensure the KML_all file has been copied to the appropriate folder created in the step above (Site and version) before proceeding.
+##Run the following code if site and section polygons need to be separated (i.e. One file from Google Earth)
+if (!require("pacman")) {install.packages("pacman")}
+pacman::p_load(sf, tidyverse, install = TRUE) #Mapping and figures
+#
+kml_file <- st_read(paste0("Reference files/KML/", Site_code, "_", Version, "/", Site_code,"_all.kml")) #Load file
+Polygons <- kml_file %>% group_by(Name) %>% summarise(count = n()) #Identify all polygons
+for (i in 1:nrow(Polygons)) {
+  polygon <- kml_file %>% filter(Name == Polygons$Name[i]) #Get the current unique polygon
+  filename <- paste0("Reference files/KML/", Site_code, "_", Version, "/",Polygons$Name[i], ".kml") # Write to separate KML file
+  st_write(polygon, filename, driver = "kml")
+  print(paste("Polygon", i, "to", filename))
+}
+##Confirm all polygons needed are listed in the output. If any are missing, they are missing from the "all" file. 
+#Confirm names are accurate, updated as needed within the local directory or return to the KML file creation step.
+#
+#
