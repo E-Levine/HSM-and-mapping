381fa96fa740a38f0f13cf616a31a484c9f39784
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-01-10 14:29:25 -0500

    Update 2_HSM_creation.R
    Code updates. Able to load site file and section files. Next: limiting grid and assigning sections.

--- HSM code/2_HSM_creation.R
+++ HSM code/2_HSM_creation.R
@@ -5,18 +5,19 @@
 #graphics.off()  # turns off any plots from previous work session
 #rm(list=ls(all=TRUE)) # clears out environment 
 #
-#Load require packages (should install missing packages as necessary) - MAKE SURE PACMAN IS INSTALLED AND RUNNING!
+#Load required packages (should install missing packages as necessary) - MAKE SURE PACMAN IS INSTALLED AND RUNNING!
 if (!require("pacman")) {install.packages("pacman")}
-pacman::p_load(plyr, tidyverse, #Df manipulation, basic summary
+pacman::p_load(plyr, tidyverse, readxl, #Df manipulation, basic summary
                sf, raster, terra,
-               leaflet, tmap, 
+               leaflet, tmap, open
                install = TRUE) #Mapping and figures
 #
 #
 #Working parameters - to be set each time a new site or version is being created. Make sure to use same code and v from setup file.
 Site_Code <- c("SL") #two-letter site code
+Version <- c("v1") #Model version
 State_Grid <- c("H4")
-Alt_State_Grid <- c(NA) #Two-letter StateGrid ID, enter NA if no secondary StateGrid needed
+Alt_Grid <- c(NA) #Two-letter StateGrid ID, enter NA if no secondary StateGrid needed
 #
 #
 #
@@ -26,8 +27,20 @@ Alt_State_Grid <- c(NA) #Two-letter StateGrid ID, enter NA if no secondary State
 Grid <- st_read(paste0("Reference files/Grids/Florida_PicoGrid_WGS84_",State_Grid,"/Florida_PicoGrid_WGS84_",State_Grid,"_clip.shp"))
 if(!is.na(Alt_Grid)){Alt_MicroGrid <- st_read(paste0("Reference files/Grids/Florida_PicoGrid_WGS84_",Alt_State_Grid,"/Florida_PicoGrid_WGS84_",Alt_State_Grid,"_clip.shp"))}
 #
-#Check data, view map  to confirm area
-plot(Grid$geometry); head(Grid)
-#Check alternate
-if(!is.na(Alt_Grid)){plot(Alt_Grid$geometry); head(Alt_Grid)} else {print("No additional grid is being used.")}
+#Check grid(s), view map(s)  to confirm area
+head(Grid)
+if(!is.na(Alt_Grid)){head(Alt_Grid)} else {print("No additional grid is being used.")}
 #
+##Load site KML and section KMLs as needed
+OrderSections <- read_excel(paste0(Site_Code, "_", Version, "/Data/", Site_Code, "_", Version, "_model_setup.xlsx"), sheet = "Section_Order")
+Site_area <- st_read(paste0("Reference files/KML/",Site_Code, "_", Version,"/", Site_Code, ".kml"))
+plot(Site_area[1]) #Output site area plot
+
+SectionList <- unlist((OrderSections %>% arrange(Order))[,"KML_Name"]) #Output list of names
+for (i in seq_along(unique(SectionList))) {
+  temp <- st_read(paste0("Reference files/KML/",Site_Code, "_", Version,"/", unique(SectionList)[i], ".kml"))
+  assign(paste0("Section",i), temp)
+  
+}
+#
+#Sections have been loaded into objects for us. Next steps to limit grid cells and assign Section. Then move into function.
