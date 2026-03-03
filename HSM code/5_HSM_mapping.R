2425ec198b596de74b422c2f9c0d4e1428bc4cdb
ELevine <149699489+E-Levine@users.noreply.github.com>
2026-03-03 12:31:11 -0500

    Update 5_HSM_mapping.R
    Area and location reference maps.

--- HSM code/5_HSM_mapping.R
+++ HSM code/5_HSM_mapping.R
@@ -11,7 +11,7 @@ pacman::p_load(plyr, tidyverse, readxl, #Df manipulation, basic summary
                sf, raster, terra, fst,
                leaflet, tmap, openxlsx, writexl,
                ggrepel, extrafont,
-               classInt, BAMMtools, #Jenks
+               shadowtext,
                install = TRUE) #Mapping and figures
 #Run once to get Arial:
 #font_import(prompt = FALSE)
@@ -27,9 +27,13 @@ Version <- c("v1") #Model version
 #
 # Data setup ----
 #
-###Load shape file with model data: default shp_filename = "_datalayer"
+###Load shape file with model data: 
+model_file_name <- "HSM_model"
 # Also loads files for scoring
-HSMfunc$load_model_files(shp_filename = "HSM_model")
+HSMmodel <- st_read(paste0(Site_Code,"_", Version, "/Output/Shapefiles/", Site_Code,"_", Version, "_", model_file_name, ".shp"))
+HSM_scores_t <- read_csv(paste0(Site_Code,"_", Version, "/Output/Data files/", Site_Code,"_", Version, "_model_scores.csv"))
+HSM_scores <- left_join(HSMmodel, 
+                        HSM_scores_t)
 #
 # Model area
 Site_area <- st_read(paste0(Site_Code,"_", Version, "/Data/Layers/KML/", Site_Code, ".kml"))
@@ -39,16 +43,79 @@ plot(Site_area[2])
 FL_outline <- st_read("Data layers/FL_Outlines/FL_Outlines.shp")
 plot(FL_outline)
 #
+# Reference locations
+Ref_locs <- read_xlsx("Data layers/Important_locations.xlsx", sheet = 1, .name_repair = "universal")
+#
+#
+# Foramtting ----
+#
+#
+basetheme <- theme_classic()+
+  theme(
+  panel.border = element_rect(color = "black", fill = NA, linewidth = 0.5),
+  axis.title = element_blank(),#element_text(size = 14, color = "black"), 
+  axis.text =  element_text(size = 15, color = "black"),
+  axis.text.x = element_text(angle = 30, vjust = 0.5)
+)
+#
 #
 #
 # Area map ----
 #
+(p1 <- ggplot()+
+  geom_sf(data = Site_area, fill = "#006699")+
+  geom_sf(data = FL_outline, linewidth = 0.35)+
+  geom_point(data = Ref_locs, aes(Longitude, Latitude), alpha = 0.8, size = 4)+
+  geom_shadowtext(data = Ref_locs, aes(Longitude, Latitude, label = Location),
+            nudge_x = 0.0225, nudge_y = -0.005, # ADJUST AS NEEDED
+            size = 5, fontface = "bold", family = "Arial", 
+            color = "black", bg.color = "white")+
+  annotate("text", label = "Atlantic\nOcean", x = -80.12, y = 27.25, # PDATE AS NEEDED
+           fontface = "italic", size = 5)+
+  basetheme +
+   theme(panel.background = element_rect(fill = "#CCFFFF"))+
+  coord_sf(xlim = c(st_bbox(Site_area)["xmin"]-0.05, st_bbox(Site_area)["xmax"]+0.10),
+           ylim = c(st_bbox(Site_area)["ymin"]-0.05, st_bbox(Site_area)["ymax"]+0.05)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_area_map.png"),
+  plot = p1,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+# 
+#
+# Inset/reference map ----
+#
+# Create bounding box polygon of your site (for clearer highlight if desired)
+site_bbox <- st_as_sfc(st_bbox(Site_area))
+bbox_buffer <- st_buffer(site_bbox, dist = 0.07)
+(p2 <- ggplot() +
+    geom_sf(data = FL_outline, fill = "grey90", color = "black", linewidth = 0.3) +
+    geom_sf(data = bbox_buffer, 
+            fill = "#CC0033",
+            color = "#CC0033",
+            linewidth = 1)+
+    theme_void()+
+    theme(panel.border =  element_rect(fill = "white", color = "black", linewidth = 0.35)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_location_map.png"),
+  plot = p2,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+#
+#
+# Data layers ----
+#
 ggplot()+
-  geom_sf(data = Site_area, fill = "#99CCFF")+
-  geom_sf(data = FL_outline)+
-  theme_classic()+
-  theme(panel.border = element_rect(color = "black", fill = NA), 
-        axis.title = element_text(size = 12, color = "black"), 
-        axis.text =  element_text(size = 10, color = "black"))+
-  coord_sf(xlim = c(st_bbox(Site_area)["xmin"]-0.05, st_bbox(Site_area)["xmax"]+0.15),
-           ylim = c(st_bbox(Site_area)["ymin"]-0.05, st_bbox(Site_area)["ymax"]+0.05))
+  geom_sf(data = HSM_scores, aes(fill = OystAV, color = OystAV))+
+  basetheme+
+  scale_fill_viridis_c() + scale_color_viridis_c()
+
+#
