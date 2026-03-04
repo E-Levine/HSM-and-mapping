2512fa2a0cf5610310992a6edb85ccfdf0e2c383
ELevine <149699489+E-Levine@users.noreply.github.com>
2026-03-04 15:23:05 -0500

    Update 5_HSM_mapping.R
    Added maps for interpolated data and composite model info.
    Added ability to load multiple shp files into one.
    Updated for SS models.

--- HSM code/5_HSM_mapping.R
+++ HSM code/5_HSM_mapping.R
@@ -21,7 +21,7 @@ HSMfunc <- new.env()
 source("HSM code/Functions/HSM_scoring_functions.R", local = HSMfunc)
 #
 #Working parameters - to be set each time a new site or version is being used Make sure to use same Site_code and Version number from setup file.
-Site_Code <- c("SL") #two-letter site code
+Site_Code <- c("SS") #two-letter site code
 Version <- c("v1") #Model version
 #
 #
@@ -29,9 +29,18 @@ Version <- c("v1") #Model version
 #
 ###Load shape file with model data: 
 model_file_name <- "HSM_model"
+model_scores_date <- c("2026-02-05")#c("2026-03-04")
 # Also loads files for scoring
-HSMmodel <- st_read(paste0(Site_Code,"_", Version, "/Output/Shapefiles/", Site_Code,"_", Version, "_", model_file_name, ".shp"))
-HSM_scores_t <- read_csv(paste0(Site_Code,"_", Version, "/Output/Data files/", Site_Code,"_", Version, "_model_scores.csv"))
+shp_pattern <- paste0("^", Site_Code, "_", Version, "_", model_file_name, "_", model_scores_date, ".*\\.shp$")
+shp_files <- list.files(path = file.path(paste0(Site_Code, "_", Version), "Output", "Shapefiles"),
+                        pattern = shp_pattern,
+                        full.names = TRUE
+)
+HSMmodel <- shp_files %>%
+  map(st_read, quiet = TRUE) %>%
+  bind_rows()
+
+HSM_scores_t <- read_csv(paste0(Site_Code,"_", Version, "/Output/Data files/", Site_Code,"_", Version, "_model_scores_", model_scores_date, ".csv"))
 HSM_scores <- left_join(HSMmodel, 
                         HSM_scores_t)
 #
@@ -47,7 +56,7 @@ plot(FL_outline)
 Ref_locs <- read_xlsx("Data layers/Important_locations.xlsx", sheet = 1, .name_repair = "universal")
 #
 #
-# Foramtting ----
+# Formatting ----
 #
 #
 basetheme <- theme_classic()+
@@ -73,15 +82,15 @@ legendtheme <- theme(
   geom_sf(data = FL_outline, linewidth = 0.35)+
   geom_point(data = Ref_locs, aes(Longitude, Latitude), alpha = 0.8, size = 4)+
   geom_shadowtext(data = Ref_locs, aes(Longitude, Latitude, label = Location),
-            nudge_x = 0.0225, nudge_y = -0.005, # ADJUST AS NEEDED
+            nudge_x = 0.05, nudge_y = -0.025, # ADJUST AS NEEDED
             size = 5, fontface = "bold", family = "Arial", 
             color = "black", bg.color = "white")+
-  annotate("text", label = "Atlantic\nOcean", x = -80.12, y = 27.25, # PDATE AS NEEDED
+  annotate("text", label = "Atlantic\nOcean", x = -80.12, y = 27.25, # UPDATE AS NEEDED
            fontface = "italic", size = 5, family = "Arial")+
   basetheme +
    theme(panel.background = element_rect(fill = "#CCFFFF"))+
-  coord_sf(xlim = c(st_bbox(Site_area)["xmin"]-0.05, st_bbox(Site_area)["xmax"]+0.10),
-           ylim = c(st_bbox(Site_area)["ymin"]-0.05, st_bbox(Site_area)["ymax"]+0.05)))
+  coord_sf(xlim = c(st_bbox(Site_area)["xmin"]-0.05, st_bbox(Site_area)["xmax"]+0.05),
+           ylim = c(st_bbox(Site_area)["ymin"]-0.025, st_bbox(Site_area)["ymax"]+0.025)))
 #
 ggsave(
   filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_area_map.png"),
@@ -97,7 +106,7 @@ ggsave(
 #
 # Create bounding box polygon of your site (for clearer highlight if desired)
 site_bbox <- st_as_sfc(st_bbox(Site_area))
-bbox_buffer <- st_buffer(site_bbox, dist = 5000)
+bbox_buffer <- st_buffer(site_bbox, dist = 0)
 (p2 <- ggplot() +
     geom_sf(data = FL_outline, fill = "grey90", color = "black", linewidth = 0.3) +
     geom_sf(data = bbox_buffer, 
@@ -124,7 +133,7 @@ ggsave(
 (p3 <- ggplot()+
   geom_sf(data = HSM_scores, aes(color = OystAV)) +
   basetheme + legendtheme +
-  scale_color_viridis_c()+
+  scale_color_viridis_c(limits = c(0,1))+
   labs(color = "Oyster habitat") + # UPDATE AS NEEDED
   theme(axis.text.x = element_text(angle = 0, vjust = 0)))
 #
@@ -142,7 +151,7 @@ ggsave(
 (p4 <- ggplot()+
     geom_sf(data = HSM_scores, aes(color = BuffAV)) +
     basetheme + legendtheme +
-    scale_color_viridis_c()+
+    scale_color_viridis_c(limits = c(0,1))+
     labs(color = "Oyster buffer") + # UPDATE AS NEEDED
     theme(axis.text.x = element_text(angle = 0, vjust = 0)))
 #
@@ -160,12 +169,12 @@ ggsave(
 (p5 <- ggplot()+
     geom_sf(data = HSM_scores, aes(color = SgrsAV)) +
     basetheme + legendtheme +
-    scale_color_viridis_c()+
-    labs(color = "Segrass habitat") + # UPDATE AS NEEDED
+    scale_color_viridis_c(limits = c(0,1))+
+    labs(color = "Seagrass habitat") + # UPDATE AS NEEDED
     theme(axis.text.x = element_text(angle = 0, vjust = 0)))
 #
 ggsave(
-  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_Segrass.png"),
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_Seagrass.png"),
   plot = p5,
   width = 9,
   height = 5,
@@ -185,6 +194,101 @@ ggsave(
 ggsave(
   filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_Channels.png"),
   plot = p5,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+#
+#
+# Interpolation layer scores ----
+#
+# Salinity
+(p6 <- ggplot()+
+   geom_sf(data = HSM_scores, aes(color = SAV)) +
+   basetheme + legendtheme +
+   scale_color_viridis_c(limits = c(0,1))+
+   labs(color = "Salinity") + # UPDATE AS NEEDED
+   theme(axis.text.x = element_text(angle = 0, vjust = 0)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_Salinity.png"),
+  plot = p6,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+#
+#
+# Temperature
+(p7 <- ggplot()+
+    geom_sf(data = HSM_scores, aes(color = TAV)) +
+    basetheme + legendtheme +
+    scale_color_viridis_c(limits = c(0,1))+
+    labs(color = "Temperature") + # UPDATE AS NEEDED
+    theme(axis.text.x = element_text(angle = 0, vjust = 0)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_Temperature.png"),
+  plot = p7,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+#
+#
+#
+# Composite model ----
+#
+## HSM groups
+(p8 <- ggplot()+
+    geom_sf(data = HSM_scores, aes(color = HSMgrp, fill = HSMgrp)) +
+    basetheme + legendtheme +
+    scale_color_viridis_d()+ scale_fill_viridis_d()+
+    labs(color = "HSM score", fill = "HSM score") + # UPDATE AS NEEDED
+    theme(axis.text.x = element_text(angle = 0, vjust = 0)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_Composite.png"),
+  plot = p8,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+#
+#
+## Jenks breaks
+(p9 <- ggplot()+
+    geom_sf(data = HSM_scores, aes(color = HSMjb, fill = HSMjb)) +
+    basetheme + legendtheme +
+    scale_color_viridis_d()+ scale_fill_viridis_d()+
+    labs(color = "Jenks breaks", fill = "Jenks breaks") + # UPDATE AS NEEDED
+    theme(axis.text.x = element_text(angle = 0, vjust = 0)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_JenksBreaks.png"),
+  plot = p9,
+  width = 9,
+  height = 5,
+  units = "in",
+  dpi = 300 # Use 300 dpi for high quality
+)
+#
+#
+## Quartile breaks
+(p10 <- ggplot()+
+    geom_sf(data = HSM_scores, aes(color = HSM_q4, fill = HSM_q4)) +
+    basetheme + legendtheme +
+    scale_color_viridis_d()+ scale_fill_viridis_d()+
+    labs(color = "Quartile breaks", fill = "Quartile breaks") + # UPDATE AS NEEDED
+    theme(axis.text.x = element_text(angle = 0, vjust = 0)))
+#
+ggsave(
+  filename = paste0(Site_Code,"_", Version, "/Output/Map files/",Site_Code,"_", Version,"_QuartileBreaks.png"),
+  plot = p10,
   width = 9,
   height = 5,
   units = "in",
